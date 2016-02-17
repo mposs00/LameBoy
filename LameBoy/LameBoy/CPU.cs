e5e79a689b131701251953386f774029a6e257ab
ASSympt0te <dentonposs@gmail.com>
2016-02-17 00:47:42 -0600

    more instructions

--- LameBoy/LameBoy/CPU.cs
+++ LameBoy/LameBoy/CPU.cs
@@ -8,7 +8,11 @@ namespace LameBoy
 {
     class CPU
     {
-        int pc = 0x100;
+        short pc = 0x100;
+        byte a = 0;
+        byte b = 0;
+        byte c = 0;
+        short hl = 0;
         Cart cart;
         byte instr;
 
@@ -27,6 +31,40 @@ namespace LameBoy
                 {
                     pc++;
                     Console.WriteLine("NOP");
+                }
+                else if(instr == 0x05)
+                {
+                    b--;
+                    pc++;
+                    Console.WriteLine("Decremented b to 0x" + b.ToString("X2"));
+                }
+                else if(instr == 0x06)
+                {
+                    byte data = cart.Read8(pc + 1);
+                    b = data;
+                    pc += 2;
+                    Console.WriteLine("Loaded value 0x" + data.ToString("X2") + " into b, advanced to $" + pc.ToString("X4"));
+                }
+                else if(instr == 0x0E)
+                {
+                    byte data = cart.Read8(pc + 1);
+                    c = data;
+                    pc += 2;
+                    Console.WriteLine("Loaded value 0x" + data.ToString("X2") + " into c, advanced to $" + pc.ToString("X4"));
+                }
+                else if(instr == 0x21)
+                {
+                    short data = cart.Read16(pc + 1);
+                    hl = data;
+                    pc += 3;
+                    Console.WriteLine("Loaded value 0x" + data.ToString("X4") + " into hl");
+                }
+                else if(instr == 0xAF)
+                {
+                    byte result = (byte) (a ^ a);
+                    a = result;
+                    pc++;
+                    Console.WriteLine("XORed a with a, resulting in a containing 0x" + a.ToString("X2"));
                 }
                 else if(instr == 0xC3)
                 {
