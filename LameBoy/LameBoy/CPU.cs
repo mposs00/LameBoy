579b4cf897a41bcd8569e6069baf22295e4664b0
ASSympt0te <dentonposs@gmail.com>
2016-02-18 09:44:59 -0600

    implement a few instrs, add zero flags to dec b

--- LameBoy/LameBoy/CPU.cs
+++ LameBoy/LameBoy/CPU.cs
@@ -12,7 +12,9 @@ namespace LameBoy
         byte a = 0;
         byte b = 0;
         byte c = 0;
+        byte f = 0;
         short hl = 0;
+        bool zeroSet = false;
         Cart cart;
         byte instr;
 
@@ -25,8 +27,11 @@ namespace LameBoy
         {
             while (true)
             {
+                //TODO: Properly set the flags on each instruction
+                //TODO: Don't output the PC every time it changes, only once on each CPU cycle
                 instr = cart.Read8(pc);
-                Console.WriteLine("PC: 0x" + instr.ToString("X2"));
+                Console.WriteLine("PC: $" + pc.ToString("X4"));
+                Console.WriteLine("Instruction: 0x" + instr.ToString("X2"));
                 if (instr == 0x00)
                 {
                     pc++;
@@ -36,6 +41,10 @@ namespace LameBoy
                 {
                     b--;
                     pc++;
+                    if (b == 0x00)
+                        f += 0x40; //0x40 = 0b01000000, zero flag
+                    else
+                        f = 0;
                     Console.WriteLine("Decremented b to 0x" + b.ToString("X2"));
                 }
                 else if(instr == 0x06)
@@ -52,12 +61,47 @@ namespace LameBoy
                     pc += 2;
                     Console.WriteLine("Loaded value 0x" + data.ToString("X2") + " into c, advanced to $" + pc.ToString("X4"));
                 }
+                else if (instr == 0x20)
+                {
+                    if(!((f & 0x40) == 0x40))
+                    {
+                        //Relative jumps are stored as a two's complement binary number, and must account for the instruction
+                        //just executed, so the PC must also be incremented
+                        pc++;
+                        byte loc = cart.Read8(pc);
+                        bool negative = ((loc & 0x80) == 0x80);
+                        if (negative)
+                        {
+                            loc = (byte) (255 - loc);
+                            pc -= loc;
+                        }
+                        else
+                        {
+                            pc += loc;
+                        }
+                        Console.WriteLine("PC advanced by 0x" + loc.ToString("X2") + " to address $" + pc.ToString("X4"));
+                    }
+                    else
+                    {
+                        pc++;
+                        Console.WriteLine("Zero flag set, PC incremented");
+                    }
+                    f = 0;
+                }
                 else if(instr == 0x21)
                 {
                     short data = cart.Read16(pc + 1);
                     hl = data;
                     pc += 3;
                     Console.WriteLine("Loaded value 0x" + data.ToString("X4") + " into hl");
+                }
+                else if(instr == 0x32)
+                {
+                    //TODO: Ram interface, properly implement this instruction (ldd)
+                    //copy a to address stored in hl
+                    Console.WriteLine("a copied to $" + hl.ToString("X2") + " and HL decremented");
+                    hl--;
+                    pc++;
                 }
                 else if(instr == 0xAF)
                 {
